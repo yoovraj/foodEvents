0264fe5a6de0aec05e692bec21c0a0dfa65ad4ee
yashinde <yashinde@gmail.com>
2015-07-07 21:49:56 +0900

    Test Api

--- recipe-api/src/main/java/com/my/food/recipe/api/resource/RecipeResource.java
+++ recipe-api/src/main/java/com/my/food/recipe/api/resource/RecipeResource.java
@@ -27,16 +27,63 @@ import org.slf4j.LoggerFactory;
  */
 @Path("recipe")
 public class RecipeResource {
-    
+
     static final Logger LOG = LoggerFactory.getLogger(RecipeResource.class);
-    
+
+    @Path("/test")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Recipe test() {
+        Recipe recipe = new Recipe();
+        RecipeSummary recipeSummary = new RecipeSummary();
+        recipeSummary.setRecipeId("1000");
+        recipeSummary.setCookTime(100);
+        recipeSummary.setCookingMethod(101);
+        recipeSummary.setNutrition("wheat");
+        recipeSummary.setPrepTime(10000l);
+        recipeSummary.setRecipeCategory("Lunch");
+        recipeSummary.setRecipeCuisine("Indian");
+        recipeSummary.setRecipeYield(10);
+        recipeSummary.setCommentCount(50);
+        recipeSummary.setPerson("Test");
+        recipeSummary.setDateCreated(System.currentTimeMillis() / 1000);
+        recipeSummary.setDateModified(System.currentTimeMillis() / 1000);
+        recipeSummary.setImageUrl("image");
+        recipeSummary.setVideUrl("video");
+
+        recipe.setRecipeSummary(recipeSummary);
+
+        RecipeDetails recipeDetails = new RecipeDetails();
+        recipeDetails.setRecipeId("1000");
+        recipeDetails.setRecipeIngredient(new String[]{"item1", "item2"});
+        recipeDetails.setRecipeInstructions(new String[]{"step1", "step2"});
+        recipeDetails.setDescription("Description");
+        
+        recipe.setRecipeDetails(recipeDetails);
+        
+        Comment comment1 = new Comment();
+        comment1.setRecipeId("1000");
+        comment1.setCommentId("1");
+        comment1.setComment("Comment 1 ");
+        comment1.setCommentTime(System.currentTimeMillis()/1000);
+        
+        Comment comment2 = new Comment();
+        comment2.setRecipeId("1000");
+        comment2.setCommentId("2");
+        comment2.setComment("Comment 2 ");
+        comment2.setCommentTime(System.currentTimeMillis()/1000);
+        
+        recipe.setComments(new Comment[] {comment1, comment2});
+        return recipe;
+    }
+
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Recipe putRecipe(Recipe recipe) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/summary")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -45,15 +92,15 @@ public class RecipeResource {
             @PathParam("recipeId") String recipeId,
             RecipeSummary recipeSummary) {
         long start = System.currentTimeMillis();
-        LOG.debug("recipeId={}, recipeSummary={}" , recipeId, recipeSummary.toString());
+        LOG.debug("recipeId={}, recipeSummary={}", recipeId, recipeSummary.toString());
         recipeSummary.setRecipeId(recipeId);
         MongoDb dbConnection = new MongoDb();
         dbConnection.putRecipeSummary(recipeSummary);
         RecipeSummary updatedSummary = dbConnection.getRecipeSummary(recipeId);
-        
+
         Recipe recipe = new Recipe();
         recipe.setRecipeSummary(updatedSummary);
-        LOG.info("POST /" + recipeId + "/summary : " + (System.currentTimeMillis() - start) +" ms");
+        LOG.info("POST /" + recipeId + "/summary : " + (System.currentTimeMillis() - start) + " ms");
         return recipe;
     }
 
@@ -66,7 +113,7 @@ public class RecipeResource {
             RecipeDetails recipeDetails) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/comments")
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
@@ -76,7 +123,7 @@ public class RecipeResource {
             Comment comment) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/{commentId}")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
@@ -87,24 +134,24 @@ public class RecipeResource {
             Comment comment) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Recipe getRecipe(@PathParam("recipeId") String recipeId) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/summary")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Recipe getRecipeSummary(@PathParam("recipeId") String recipeId) {
         long start = System.currentTimeMillis();
-        LOG.debug("recipeId={} " , recipeId);
+        LOG.debug("recipeId={} ", recipeId);
         RecipeSummary recipeSummary = new MongoDb().getRecipeSummary(recipeId);
         Recipe recipe = new Recipe();
         recipe.setRecipeSummary(recipeSummary);
-        LOG.info("POST /" + recipeId + "/summary : " + (System.currentTimeMillis() - start) +" ms");
+        LOG.info("POST /" + recipeId + "/summary : " + (System.currentTimeMillis() - start) + " ms");
         return recipe;
     }
 
@@ -114,14 +161,14 @@ public class RecipeResource {
     public Recipe getRecipeDetails(@PathParam("recipeId") String recipeId) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/comments")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Recipe getRecipeComments(@PathParam("recipeId") String recipeId) {
         return new Recipe();
     }
-    
+
     @Path("/{recipeId}/{commentId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
